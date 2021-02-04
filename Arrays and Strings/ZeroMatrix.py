4ea6a139b7a69e68273006ad9d53cf0ae85c766e
bazzaza <waelbazzaza@gmail.com>
2021-02-05 00:40:27 +0400

    Modified ZeroMatrix.py to include an O(m+n) space solution

--- Arrays and Strings/ZeroMatrix.py
+++ Arrays and Strings/ZeroMatrix.py
@@ -1,24 +1,38 @@
 import copy
 
-def ZeroMatrix(m):
+def ZeroMatrix(matrix):
 
-    n = copy.deepcopy(m)
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    row_set = set()
+    col_set = set()
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 0:
+                row_set.add(i)
+                col_set.add(j)
+
+    for i in range(rows):
+        for j in range(cols):
+            if i in row_set or j in col_set:
+                matrix[i][j] = 0
 
 
-    for i in range(len(m)):
-        for j in range(len(m[0])):
-            if m[i][j] == 0:
-                n[i] = [0 for item in n[i]]
-                for r in n:
-                    r[j] =0
 
-    return n
+
 
 
 
 matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
-matrix = [[1,1,1],[1,0,1],[1,1,1]]
+#matrix = [[1,1,1],[1,0,1],[1,1,1]]
 
+print("Before:")
+for row in matrix:
+    print(row)
 result = ZeroMatrix(matrix)
-print(result)
-print(matrix)
+
+print("After:")
+for row in matrix:
+    print(row)
